56c66e9adac9c42e7a02029e8350a949602f28e6
Fábio Domingues <domingues.fabio2002@gmail.com>
2025-04-24 09:32:05 -0300

    Imprementação de classe e teste no Program

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
+    using Models;
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia:Smartphone
     {
+        public Nokia (string numero, string imei, string modelo, int memoria) : base(numero, imei, modelo, memoria){
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Aplicativo {nomeApp} instalado com sucesso");
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("47912341234","1234","Nokia 5", 32);
+Iphone iphone= new Iphone("47912341234","1234","Iphone 7", 64);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
